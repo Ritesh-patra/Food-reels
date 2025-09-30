0f491fe403e625eb8b23e08cf7c9076b26a44cde
Ritesh-patra <patrasagarika654@gmail.com>
2025-09-30 18:09:44 +0530

    change in db

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -1,15 +1,18 @@
 const mongoose = require('mongoose');
 
-
-
 function connectDB() {
-    mongoose.connect(process.env.MONGODB_URI)
-        .then(() => {
-            console.log("MongoDB connected");
-        })
-        .catch((err) => {
-            console.log("MongoDB connection error:", err);
-        })
+    mongoose.connect(process.env.MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        tls: true, // TLS/SSL enable
+        tlsAllowInvalidCertificates: false, // true if self-signed certificate
+    })
+    .then(() => {
+        console.log("MongoDB connected");
+    })
+    .catch((err) => {
+        console.log("MongoDB connection error:", err);
+    });
 }
 
 module.exports = connectDB;
