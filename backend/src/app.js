3f2a8ca23a4ad39d222c86542b8db12e8de4c66a
Ritesh-patra <patrasagarika654@gmail.com>
2025-09-30 20:38:08 +0530

    all done

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors({
     origin: [
         "http://localhost:5173",
-        "https://food-reels-1.onrender.com/"
+        "https://food-reels.onrender.com"
     ],
     credentials: true
 }));
